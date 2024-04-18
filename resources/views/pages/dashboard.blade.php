55dfc70e718cb5d30a9f362c403bd7de3f5e7822
Dulon <dulon.iubat@gmail.com>
2024-04-17 19:34:37 -0700

    update home page

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -4,8 +4,9 @@
      <div class="card">
           <div class="card-body">
                <h2>
-                    Welcome to Dashboard..........!
+                    Welcome to Dashboard!
                </h2>
+               <p>Home</p>
           </div>
      </div>
 </div>
